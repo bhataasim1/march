69b6ab4d826095a9040780e6e125964745a7a335
Xron Trix <xrontrix@gmail.com>
2024-08-20 01:59:26 +0530

    feat: added custom svg icons

---
+++ apps/frontend/src/lib/icons/Plus.tsx
@@ -0,0 +1,35 @@
+import { type FC } from "react"
+
+interface Props {
+  size?: number
+}
+
+const Plus: FC<Props> = ({ size = 24 }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width={size}
+      height={size}
+      color="currentColor"
+      fill={"none"}
+    >
+      <path
+        d="M12 4V20"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M4 12H20"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default Plus
